2acb0e36478a7ebed25d73e1ee5021aaa0f1dfda
Stefano Sansone <stefanosansone.dev@gmail.com>
2024-02-08 11:38:26 +0000

    move strings in bundle properties

--- src/main/kotlin/com/github/stefanosansone/intellijtargetprocessintegration/utils/Constants.kt
+++ src/main/kotlin/com/github/stefanosansone/intellijtargetprocessintegration/utils/Constants.kt
@@ -1,3 +1,5 @@
 package com.github.stefanosansone.intellijtargetprocessintegration.utils
 
 const val SETTINGS_ID = "tpi.project.settings"
+const val GITHUB_ISSUES_URL = "https://github.com/stefanosansone/intellij-targetprocess-integration/issues"
+const val FEEDBACK_MAIL = "mailto:stefanosansone.dev@gmail.com?subject=TargetProcess%20IntelliJ%20Plugin%20Feedback"
